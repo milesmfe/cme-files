ec0225ec749de4ec37350d9366a419ccc6ab0df4
Miles Edwards <miles.mfe@gmail.com>
2025-06-15 12:44:48 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`)
+});
